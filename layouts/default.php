c6ef45d3c7dd905b942c52febf25b5d9365456e3
Joshua Britt <joshua.britt@pm-international.com>
2019-08-28 17:54:37 -0500

    Added discord favicon to head.

--- layouts/default.php
+++ layouts/default.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Leatherbound Book Club</title>
+    <link href="<?= path(['IMAGES', 'discord.ico']); ?>" rel="icon" />
     <?= STYLES; ?>
   </head>
   <body>
